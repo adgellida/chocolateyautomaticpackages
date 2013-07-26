60ae19296e31c024d3cb9f41e0d2e595ca84b157
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-07-27 00:36:30 +0200

    Updating

--- _output/bleachbit/0.9.6/tools/chocolateyInstall.ps1
+++ _output/bleachbit/0.9.6/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'bleachbit'
 $installerType = 'EXE'
 #$url = 'http://katana.oooninja.com/bleachbit/{version}final/download.php'
-$url = '{{DownloadUrl}}'
+$url = 'http://katana.oooninja.com/bleachbit/0.9.6final/download.php'
 $silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
