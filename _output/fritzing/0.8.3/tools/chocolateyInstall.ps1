e5fe2aa8a60c462fdba2a68091072d88f0ed965d
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-07-28 00:56:22 +0200

    Updating

--- _output/fritzing/0.8.3/tools/chocolateyInstall.ps1
+++ _output/fritzing/0.8.3/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿$packageName = 'fritzing'
 $fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\{anchor1}.zip"
 #$url = 'http://fritzing.org/download{version}b/windows/{anchor1}.zip'
-$url = '{{DownloadUrl}}'
-$exeFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\{anchor1}\fritzing.exe"
+$url = 'http://fritzing.org/download0.8.3b/windows/fritzing.2013.07.27.pc.zip'
+$exeFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\{anchor1}\fritzing"
+$path = 'C:\Chocolatey\_work\fritzing.2013.07.26.pc.zip'
 Get-ChocolateyWebFile $packageName $fileFullPath $url
 Get-ChocolateyUnzip "$fileFullPath" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyDesktopLink $exeFullPath
+#Install-ChocolateyDesktopLink $exeFullPath
