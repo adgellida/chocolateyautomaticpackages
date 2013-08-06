2192678a99309754ef620413dd56018864742521
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-06 23:25:15 +0200

    Updating

--- MULTIINSTALL.ps1
+++ MULTIINSTALL.ps1
@@ -1,82 +1,110 @@
 ####multi-installer, select programs to install 
-####v.2013.07.21 alpha 13:22 GMT+1
+####v.2013.08.07 alpha 00:00 GMT+1
 
-##NEEDS UPDATING!! WAIT FOR A NEW VERSION
+####Advices
+#TECHSHARE ROOM FEED - sufix "_tsr" to differenciate it from feed oficial - Realized by tonigellida
 
-############OFFICIAL CHOCOLATEY FEED
+#Backup & Sync
+#'goodsync_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
-##Recommended
-
-'utorrent','adobereader','adobeair' | %{ cinst $_ }
-'javaruntime' | %{ cinst $_ }
-#'teamviewer','filezilla' | %{ cinst $_ }
-'silverlight','notepadplusplus.install','vlc' | %{ cinst $_ }
-
-##Interesting
-
+#OS & Utilities
 #'OpenHardwareMonitor','microsoft.isobackuptool' | %{ cinst $_ }
-#'flashplayerplugin','flashplayeractivex' | %{ cinst $_ }
-#'skype','speccy','spotify','smartgit','Secunia.PSI','greenshot','windirstat' | %{ cinst $_ }
-#'imgburn' | %{ cinst $_ }
-#'yumi','nmap','truecrypt','tor-browser','cyg-get','microsoft.isobackuptool' | %{ cinst $_ }
-
-##Optionals
-
-'googlechrome.dev' | %{ cinst $_ }
-#'virtualbox','openal' | %{ cinst $_ }
-#'midori-browser','peazip','cpu-z','trillian','line','chromium' | %{ cinst $_ }
-
-##Recommended - tonigellida packages
-
-'k-litecodecpackbasic','unitywebplayer','ccleaner' | %{ cinst $_ }
-
-##Interesting - tonigellida packages
-
-'adobeshockwaveplayer' | %{ cinst $_ }
-#'kcleaner','sumo','littleregistrycleaner','atubecatcher' | %{ cinst $_ }
-#'songr','spybot','superantispyware','ccenhancer','speedfan' | %{ cinst $_ }
-#'avastbrowsercleanup','crystaldiskinfo','easybcd','llftool','winlockless' | %{ cinst $_ }
-#'messengerreviver','hpusbdisk','malwarebytes','systemninja','bleachbit','genymotion' | %{ cinst $_ }
-#'mousecontroller','tinytask','wassapp','viber','advancedsystemcarefree','oswelldevcpp','tuneuputilities' | %{ cinst $_ }
+#'yumi','nmap','truecrypt','tor-browser','cyg-get' | %{ cinst $_ }
+#'sumo','softwareinformer','cnettechtracker' | %{ cinst $_ }
+#'ccleaner','kcleaner','littleregistrycleaner' | %{ cinst $_ }
+#'ccenhancer','speedfan' | %{ cinst $_ }
+#'crystaldiskinfo','easybcd','llftool' | %{ cinst $_ }
+#'classicshell','startmenureviver' | %{ cinst $_ }
+#'hpusbdisk','systemninja','bleachbit','genymotion' | %{ cinst $_ }
+#'mousecontroller','tinytask','wassapp','advancedsystemcarefree','tuneuputilities' | %{ cinst $_ }
 #'driverbooster','startmenu8' | %{ cinst $_ }
 #'windowsrepair','simplesystemtweaker','advancedsystemtweaker','rmprepusb' | %{ cinst $_ }
+#'silverlight','notepadplusplus.install','teamviewer' | %{ cinst $_ }
+#'flashplayerplugin','flashplayeractivex' | %{ cinst $_ }
+#'speccy','Secunia.PSI','greenshot', | %{ cinst $_ }
+#'cpu-z','virtualbox','openal' | %{ cinst $_ }
+#'unlocker_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }	  #In official feed unlisted
+#'dumeter_tsr','hdtunepro_tsr','aria2_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+#'partitionwizard_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }	#In official feed doesn't work
+#'rollbackrx_tsr','startisback_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+#'drivergenius_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+#'aida64extreme_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
-##Optionals - tonigellida packages
+#Security & Privacy
+#'spybot','superantispyware','avastbrowsercleanup','winlockless' | %{ cinst $_ }
+#'malwarebytes' | %{ cinst $_ }
+#'kasperskyanti-virus_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
-#'xonotic','redeclipse','geany','mro','ares','meld','winmerge','emule','startmenureviver' | %{ cinst $_ }
-#'tiggit','daemontoolslite','mucommander','windowsessentials' | %{ cinst $_ }
+#CD/DVD Tools
+#'imgburn' | %{ cinst $_ }
+#'ultraiso_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+#'daemontoolslite' | %{ cinst $_ }
+
+#Photos & Graphics
+#'designsparkpcb','kicad','fritzing' | %{ cinst $_ }
+#'adobephotoshop_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+
+#Audio & Music
+'songr','spotify' | %{ cinst $_ }
+
+#Videos & Movies
+'vlc' | %{ cinst $_ }
+
+#Audio & Music & Videos & Movies
+'adobeshockwaveplayer','adobeair','k-litecodecpackbasic','unitywebplayer' | %{ cinst $_ }
+#'windowsessentials' | %{ cinst $_ }
+#'atubecatcher' | %{ cinst $_ }
+#'adobepremierepro_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+
+#Development
+'javaruntime' | %{ cinst $_ }
+#'Dogtail.DotNet3.5SP1','DotNet4.5','DotNet4.0' | %{ cinst $_ }
+#'geany','meld','winmerge' | %{ cinst $_ }
 #'arduinoide','arduinoide -Version 1.5.2.20130614-beta','arduinoide -Version 1.5.3.20130614-nightly' | %{ cinst $_ }
-#'designsparkpcb' | %{ cinst $_ }
+#'orwelldevcpp','smartgit','unity' | %{ cinst $_ }
+
+#Web Browsers
+'googlechrome' | %{ cinst $_ }
+#'googlechrome.dev' | %{ cinst $_ }
+#'midori-browser','chromium' | %{ cinst $_ }
+#'adblockpluschrome','adblockplusie','adblockplusfirefox','adblockplusopera' | %{ cinst $_ }
+
+#Social
+#'messengerreviver','viber' | %{ cinst $_ }
+#'trillian','line','skype' | %{ cinst $_ }
+
+#File Management
+#'winrar' | %{ cinst $_ }
+#'filezilla' | %{ cinst $_ }
+#'winrar_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+#'jdownloader_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+#'peazip' | %{ cinst $_ }
+#'windirstat' | %{ cinst $_ }
+#'mucommander' | %{ cinst $_ }
+
+#File Sharing
+#'ares','utorrent','emule' | %{ cinst $_ }
+#'wget_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+
+#News & Books
+
+
+
+#Office & Productivity
+'adobereader' | %{ cinst $_ }
+#'microsoftoffice_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+
+#Games
+#'mro','xonotic','redeclipse','tiggit','heroesofnewerth' | %{ cinst $_ }
+#'streetsofrage_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
+
+##NOTES
 
 #Not recommended to install, it isn't full versions or there's better alternatives - tonigellida packages
 
 #'hdtune','drivergenius','rollbackrx' | %{ cinst $_ }
 #'avastfreeantivirus','avgantivirusfree','avirafreeantivirus','kasperskyanti-virus' | %{ cinst $_ }
 #'ultraiso','goodsync','dumeter','aida64extreme' | %{ cinst $_ }
-
-############TECHSHARE ROOM FEED - sufix "_tsr" to differenciate it from feed oficial - Realized by tonigellida
-
-##Recommended
-
-'wget_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-'winrar_tsr','jdownloader_tsr','aida64extreme_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-
-##Optionals
-#'microsoftoffice_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'goodsync_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-'drivergenius_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'rollbackrx_tsr','startisback_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'adobephotoshop_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'dumeter_tsr','hdtunepro_tsr','aria2_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'partitionwizard_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }	#In official feed doesn't work
-#'unlocker_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }	  #In official feed unlisted
-#'streetsofrage_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'kasperskyanti-virus_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'ultraiso_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-
-##Coming soon on techshare room
-
-#'adobepremierepro_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
 ##Coming soon on techshare room - not priority
 
