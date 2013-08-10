f065cc65c6d78ee74ad691aad581a9e33303dcf4
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-10 17:30:43 +0200

    updating

--- songrf/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-#$url = 'http://www.vtaskstudio.com/tinytask.exe' # For testing purposes
-$url = '{{DownloadUrl}}'
-$fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\tinytask.exe"
-
-Get-ChocolateyWebFile $packageName $fileFullPath $url
-
-Install-ChocolateyDesktopLink $fileFullPath
