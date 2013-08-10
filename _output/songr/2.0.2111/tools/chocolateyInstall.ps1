437884fb624c8e1eebb848ec2f11347e7ddc020f
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-11 01:25:46 +0200

    Updating

--- _output/songr/2.0.2111/tools/chocolateyInstall.ps1
+++ _output/songr/2.0.2111/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'songr'
 $installerType = 'EXE'
 #$url = 'http://www.xamasoft.com/downloads/Songr-2.zip'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.xamasoft.com/downloads/Songr-2.zip'
 $silentArgs = '/S /NCRC'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
--- spybot/tools/chocolateyInstall.ps1
+++ spybot/tools/chocolateyInstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
-#$url = 'http://spybotupdates.com/files/spybotsd-2.1.20-SR1.exe'
-#$url = 'http://spybotupdate.biz/files/spybotsd-2.1.20-SR1.exe'
+#$url = 'http://spybotupdate.biz/files/{anchor1}.exe'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
