67665cf0735923e05b0422a24bef3f35c914a6da
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-13 02:38:43 +0200

    Updating

--- hdtune/tools/chocolateyInstall.ps1
+++ hdtune/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
-﻿$packageName = 'hdtune'
+﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
-$url = 'http://www.hdtune.com/files/hdtune_255.exe'
+#$url = 'http://www.hdtune.com/files/{anchor1}.exe'
+$url = '{{DownloadUrl}}'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
