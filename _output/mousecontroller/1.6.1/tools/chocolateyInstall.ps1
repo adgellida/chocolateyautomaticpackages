a2d394abc2e006f66479741f7682bbd2ce986f76
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-16 20:02:14 +0200

    Updating

---
+++ _output/mousecontroller/1.6.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'mousecontroller'
+#$url = 'http://switch.dl.sourceforge.net/project/mousecontroller/MouseController_{version}.zip'
+$url = 'http://switch.dl.sourceforge.net/project/mousecontroller/MouseController_1.6.1.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\MouseController.exe"
+
+Install-ChocolateyDesktopLink $targetFilePath
