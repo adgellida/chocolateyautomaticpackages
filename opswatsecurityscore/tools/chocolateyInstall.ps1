a345cba3e0f0cee5e5fd62f3bd971d86f843c5aa
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-17 10:20:43 +0200

    Updating

--- opswatsecurityscore/tools/chocolateyInstall.ps1
+++ opswatsecurityscore/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
-﻿$packageName = 'sonyvegaspro'
-$installerType = 'EXE'
-$url = 'http://dspcdn.sonycreativesoftware.com/secure/vegaspro12.0.670.exe?e=1376728983&h=7e7551645eba44f8b4220cf716f29eeb&file=.exe'
-$silentArgs = '/q /s'
+﻿$packageName = '{{PackageName}}'
+$installerType = 'msi'
+#$url = 'http://software.opswat.com/securityscore/installer/OPSWATSecurityScore.msi'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/qb'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
