10c1706196e8ff2df4abc773484d205df9037425
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-17 14:01:30 +0200

    Updating

---
+++ arduinoide/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+#$url = 'http://arduino.googlecode.com/files/arduino-{version}-windows.exe'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
