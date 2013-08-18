7338c7e56f3f74b8f4f8b5587ac4d6b61524845c
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-18 12:17:08 +0200

    Updating

--- MULTIINSTALL.ps1
+++ MULTIINSTALL.ps1
@@ -1,5 +1,5 @@
 ####multi-installer, select programs to install 
-####v.2013.08.11 alpha 00:00 GMT+1
+####v.2013.08.18 alpha 12:20 GMT+1
 
 ####Advices
 #TECHSHARE ROOM FEED - sufix "_tsr" to differenciate it from feed oficial - tonigellida apports his packages
@@ -8,23 +8,25 @@
 #'goodsync_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
 #OS & Utilities
-'ccleaner','classicshell' | %{ cinst $_ }
-#'openhardwaremonitor','microsoft.isobackuptool','yumi','cyg-get','sumo','softwareinformer','cnettechtracker' | %{ cinst $_ }
+'ccleaner','sumo','softwareinformer','teamviewer','notepadplusplus.install','speccy','silverlight' | %{ cinst $_ }
+#'classic-shellbeta','classic-shell' | %{ cinst $_ }
+#'openhardwaremonitor','microsoft.isobackuptool','yumi','cyg-get','cnettechtracker' | %{ cinst $_ }
 #'kcleaner','littleregistrycleaner','ccenhancer','speedfan','crystaldiskinfo','easybcd','llftool' | %{ cinst $_ }
 #'startmenureviver','startmenu8','driverbooster' | %{ cinst $_ }
 #'hpusbdisk','systemninja','bleachbit','genymotion','mousecontroller','tinytask','wassapp','advancedsystemcarefree','tuneuputilities' | %{ cinst $_ }
-#'windowsrepair','simplesystemtweaker','advancedsystemtweaker','rmprepusb','silverlight','notepadplusplus.install','teamviewer' | %{ cinst $_ }
-#'flashplayerplugin','flashplayeractivex','cpu-z','virtualbox','speccy','greenshot' | %{ cinst $_ }
+#'windowsrepair','simplesystemtweaker','advancedsystemtweaker','rmprepusb' | %{ cinst $_ }
+#'flashplayerplugin','flashplayeractivex','cpu-z','virtualbox','greenshot' | %{ cinst $_ }
 #'openal','ghostscript','winpcap','webpi','aria2' | %{ cinst $_ }
 #'dumeter_tsr','hdtunepro_tsr','unlocker_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 #'rollbackrx_tsr','startisback_tsr','drivergenius_tsr','aida64extreme_tsr','partitionwizard_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
 #Security & Privacy
-#'spybot','superantispyware','avastbrowsercleanup','winlockless','malwarebytes','Secunia.PSI','nmap','truecrypt','tor-browser' | %{ cinst $_ }
+#'spybot','superantispyware','avastbrowsercleanup','winlockless','malwarebytes','Secunia.PSI','nmap','truecrypt','tor-browser','piratebrowser' | %{ cinst $_ }
 #'kasperskyanti-virus_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
 #CD/DVD Tools
-#'imgburn','daemontoolslite' | %{ cinst $_ }
+'imgburn' | %{ cinst $_ }
+#'daemontoolslite' | %{ cinst $_ }
 #'ultraiso_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
 #Photos & Graphics
@@ -46,21 +48,21 @@
 'javaruntime' | %{ cinst $_ }
 #'Dogtail.DotNet3.5SP1','DotNet4.5','DotNet4.0' | %{ cinst $_ }
 #'geany','meld','winmerge' | %{ cinst $_ }
-#'arduinoide','arduinoide -Version 1.5.2.20130614-beta','arduinoide -Version 1.5.3.20130614-nightly' | %{ cinst $_ }
+#'arduinoide','arduinoidebeta','arduinoidenightly' | %{ cinst $_ }
 #'orwelldevcpp','smartgit','unity' | %{ cinst $_ }
 
 #Web Browsers
-'googlechrome' | %{ cinst $_ }
-#'googlechrome.dev','midori-browser','chromium' | %{ cinst $_ }
-#'adblockpluschrome','adblockplusie','adblockplusfirefox','adblockplusopera' | %{ cinst $_ }
+'googlechrome.dev' | %{ cinst $_ }
+#'googlechrome','chromium','midori-browser' | %{ cinst $_ }
+#'adblockpluschrome','adblockplusie' | %{ cinst $_ }
+#'adblockplusfirefox','adblockplusopera' | %{ cinst $_ }
 
 #Social
-#'messengerreviver','viber'.'trillian','line','skype' | %{ cinst $_ }
+#'messengerreviver','viber','trillian','line','skype' | %{ cinst $_ }
 
 #File Management
-#'filezilla','peazip','windirstat','mucommander' | %{ cinst $_ }
+#'filezilla','jdownloaderbeta','jdownloader','peazip','windirstat','mucommander' | %{ cinst $_ }
 #'winrar_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
-#'jdownloader_tsr' | %{ cinst $_ -Source http://www.myget.org/F/techshareroom/api/v2 }
 
 #File Sharing
 #'ares','utorrent','emule' | %{ cinst $_ }
