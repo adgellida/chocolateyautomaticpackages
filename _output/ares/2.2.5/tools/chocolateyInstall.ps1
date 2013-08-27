d05802aeac5b57723adbccf7827527b2333c73bb
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-08-27 12:56:42 +0200

    Updating

--- _output/ares/2.2.5/tools/chocolateyInstall.ps1
+++ _output/ares/2.2.5/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'ares'
 $installerType = 'EXE'
 #$url = 'http://heanet.dl.sourceforge.net/project/aresgalaxy/aresgalaxy/{anchor1}.exe'
-$url = '{{DownloadUrl}}'
+$url = 'http://heanet.dl.sourceforge.net/project/aresgalaxy/aresgalaxy/AresRegular225_072413/aresregular225_installer.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
