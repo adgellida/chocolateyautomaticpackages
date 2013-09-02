b9de8b0bc3f6e60606763298572acd80bd1be283
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-02 22:43:45 +0200

    Updating

--- _output/eagle/6.5.0/tools/chocolateyInstall.ps1
+++ _output/eagle/6.5.0/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'eagle'
 $installerType = 'EXE'
-#$url = 'http://download.aida64.com/{anchor1}.exe'
-$url = '{{DownloadUrl}}'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'ftp://ftp.cadsoft.de/eagle/program/6.5/eagle-win-6.5.0.exe'
+$silentArgs = ''
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
