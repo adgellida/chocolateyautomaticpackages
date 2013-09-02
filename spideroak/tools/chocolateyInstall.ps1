743e9e8745786c64b2bd130d92d1fc5cfca6e5b5
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-02 23:03:36 +0200

    Updating

--- spideroak/tools/chocolateyInstall.ps1
+++ spideroak/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
-#$url = 'http://download.aida64.com/{anchor1}.exe'
 $url = '{{DownloadUrl}}'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
