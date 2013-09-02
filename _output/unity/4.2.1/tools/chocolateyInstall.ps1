c628c0ea87890ccdce248635b014b568249c06f2
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-02 21:28:52 +0200

    Updating

--- _output/unity/4.2.1/tools/chocolateyInstall.ps1
+++ _output/unity/4.2.1/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'unity'
 $installerType = 'EXE'
-#$url = 'http://airdownload.adobe.com/air/win/download/{version}/AdobeAIRInstaller.exe'		#it isn't used the version variable
-$url = '{{DownloadUrl}}'
-$silentArgs = '-silent -eulaAccepted'
+#$url = 'http://download.unity3d.com/download_unity/UnitySetup-{version}.exe'
+$url = 'http://download.unity3d.com/download_unity/UnitySetup-4.2.1.exe'
+$silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
