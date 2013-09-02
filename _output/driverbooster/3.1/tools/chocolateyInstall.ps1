66881bd8ce3524ce687e716984f48be854593a8b
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-02 12:13:49 +0200

    Updating

--- _output/driverbooster/3.1/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'driverbooster'
-$installerType = 'EXE'
-#$url = 'http://download.iobit.com/driver_booster_setup.exe'
-$url = 'http://download.iobit.com/driver_booster_setup.exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
