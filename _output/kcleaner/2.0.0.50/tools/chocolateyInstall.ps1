e290fa2f6d00bfc92cc7f29baf8bb28ebafd9b82
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-04 02:05:49 +0200

    Updating

--- _output/kcleaner/2.0.0.50/tools/chocolateyInstall.ps1
+++ _output/kcleaner/2.0.0.50/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
-﻿$packageName = 'driverbooster'
+﻿$packageName = 'kcleaner'
 $installerType = 'EXE'
-$url = 'http://download.iobit.com/driver_booster_setup.exe'
+#$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
--- _output/piratebrowser/0.6/tools/chocolateyInstall.ps1
+++ _output/piratebrowser/0.6/tools/chocolateyInstall.ps1
@@ -3,15 +3,16 @@ $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $tempDir = "$env:TEMP\chocolatey\$packageName"
 #$url = 'http://piratebrowser.com/downloads/PirateBrowser_{anchor1}.exe.torrent'
 $url = 'http://piratebrowser.com/downloads/PirateBrowser_0.6b.exe.torrent'
-
+$anchor1 = '{anchor1}'
 try {
     if (-not (Test-Path $tempDir)) {New-Item $tempDir -ItemType directory}
-    $file = "$tempDir\PirateBrowser_{anchor1}.exe"
+    $fileversion = $url -replace 'http://piratebrowser.com/downloads/PirateBrowser_(+).exe.torrent', '$1'
+	$file = "$tempDir\PirateBrowser_$fileversion.exe"
 
 	aria2c --dir=$tempDir --seed-time=0 $url
     Start-Process "7za" -ArgumentList "x -o`"$installDir`" -y `"$file`"" -Wait
 
-    $targetFilePath = "$installDir\PirateBrowser {anchor1}\Start PirateBrowser.exe"
+    $targetFilePath = "$installDir\PirateBrowser $fileversion\Start PirateBrowser.exe"
 	Install-ChocolateyDesktopLink $targetFilePath
 
 } catch {
