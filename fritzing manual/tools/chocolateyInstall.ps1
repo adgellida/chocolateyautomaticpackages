9891c626e0b900a25732951d0410c82de461cb5a
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-05 16:15:44 +0200

    Updating

---
+++ fritzing manual/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿try {
+	$packageName = 'fritzing'
+    $url = 'http://fritzing.org/download/0.8.3b/windows/fritzing.2013.07.27.pc.zip'
+			 
+	$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+    Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+    $targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\fritzing.2013.07.27.pc\fritzing.exe"
+    Install-ChocolateyDesktopLink $targetFilePath
+
+    Write-ChocolateySuccess $packageName
+}   catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
