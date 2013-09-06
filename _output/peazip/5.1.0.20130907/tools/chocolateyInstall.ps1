9f1376a592e8c8f597b06ea387c182c500a1b58f
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-06 23:56:14 +0200

    Updating

--- _output/peazip/5.1.0.20130907/tools/chocolateyInstall.ps1
+++ _output/peazip/5.1.0.20130907/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'peazip'
 $installerType = 'EXE'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
@@ -6,10 +6,10 @@ $validExitCodes = @(0) #please insert other valid exit codes here, exit codes fo
 $processor = Get-WmiObject Win32_Processor
   $is64bit = $processor.AddressWidth -eq 64
   if ($is64bit) {
-    $url = '{{DownloadUrlx64}}'
+    $url = 'http://surfnet.dl.sourceforge.net/project/peazip/5.1.0/peazip-plain-5.1.0.WIN64.exe'
     #$url = 'http://surfnet.dl.sourceforge.net/project/peazip/5.1.0/peazip-plain-5.1.0.WIN64.exe'
   } else {
-    $url = '{{DownloadUrl}}'
+    $url = 'http://surfnet.dl.sourceforge.net/project/peazip/5.1.0/peazip-plain-5.1.0.WINDOWS.exe'
     #$url = 'http://surfnet.dl.sourceforge.net/project/peazip/5.1.0/peazip-plain-5.1.0.WINDOWS.exe'
   }
 
