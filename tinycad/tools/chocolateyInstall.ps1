3971682db778f85834345c536441905468dd10a4
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-08 23:20:42 +0200

    Updating

---
+++ tinycad/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+#$url = 'http://netcologne.dl.sourceforge.net/project/tinycad/Official%20Release/{version2}/TinyCAD_{version}_Production_Setup.exe'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
