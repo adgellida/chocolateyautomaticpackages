fd31e658f8017f06a75f7e30983327fb61583f0c
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-11 14:16:04 +0200

    Updating

---
+++ _output/pokerth/1.0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'pokerth'
+$installerType = 'EXE'
+#$url = 'http://heanet.dl.sourceforge.net/project/pokerth/pokerth/{version1}/PokerTH-{version}-windows-installer.exe'
+$url = 'http://heanet.dl.sourceforge.net/project/pokerth/pokerth/1.0/PokerTH-1.0.1-windows-installer.exe'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
