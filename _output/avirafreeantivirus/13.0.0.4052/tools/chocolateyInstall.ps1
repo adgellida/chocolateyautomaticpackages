2fda03a6b2d023de77ae65cd831ca14e1882406e
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-09-28 22:54:09 +0200

    Updating

--- _output/avirafreeantivirus/13.0.0.4052/tools/chocolateyInstall.ps1
+++ _output/avirafreeantivirus/13.0.0.4052/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
-﻿$packageName = 'cccp'
+﻿$packageName = 'avirafreeantivirus'
 $installerType = 'EXE'
-$url = 'http://mirror.exoware.net/cccp/Combined-Community-Codec-Pack-2013-08-01.exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+#$url = 'http://personal.avira-update.com/package/webloader/win32/pecl/avira_free_antivirus.exe'
+$url = 'http://personal.avira-update.com/package/webloader/win32/pecl/avira_free_antivirus.exe'
+$silentArgs = ''
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
--- cccp/tools/chocolateyInstall.ps1
+++ cccp/tools/chocolateyInstall.ps1
@@ -1,6 +1,5 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
-#$url = 'http://mirror.exoware.net/cccp/Combined-Community-Codec-Pack-2013-08-01.exe'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
