5591bb2f415eaff2f7efdc34381685b414446cb9
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-10-12 13:09:17 +0200

    Updating

---
+++ _output/arduinoidegalileo/0.7.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿try {
+	$packageName = 'arduinoidegalileo'
+    $url = 'http://downloadmirror.intel.com/23171/eng/Intel_Galileo_Arduino_SW_1.5.3_on_Windows_v0.7.5.zip'
+    $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+    Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+    $targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\arduino-1.5.3\arduino.exe"
+    Install-ChocolateyDesktopLink $targetFilePath
+
+    Write-ChocolateySuccess $packageName
+}   catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
