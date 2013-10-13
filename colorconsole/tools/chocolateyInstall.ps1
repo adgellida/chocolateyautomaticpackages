5d70382f87f892a5c1a1c2f4d3b640d72cfa7de8
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-10-14 01:18:03 +0200

    Updating

---
+++ colorconsole/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\ColorConsole.exe"
+
+Install-ChocolateyDesktopLink $targetFilePath
