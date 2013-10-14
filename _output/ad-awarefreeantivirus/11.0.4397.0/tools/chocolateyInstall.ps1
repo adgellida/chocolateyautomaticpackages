bebbf6b914e2cd033af0c8d31c7c1de9ee00f7d3
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-10-14 15:25:44 +0200

    Updating

--- _output/ad-awarefreeantivirus/11.0.4397.0/tools/chocolateyInstall.ps1
+++ _output/ad-awarefreeantivirus/11.0.4397.0/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'ad-awarefreeantivirus'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'http://download100.lavasoft.com/adaware/updates/Adaware_Installer_free.exe'
 $silentArgs = ''
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
