a15d7f58a0376e026687d500da0b49da9efeb743
Antonio David Gellida Lavara <tonigellida@gmail.com>
2013-11-24 09:56:44 +0100

    Updating

---
+++ jrt/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\winlockless.exe"
+
+Get-ChocolateyWebFile $packageName $fileFullPath $url
+
+Install-ChocolateyDesktopLink $fileFullPath
