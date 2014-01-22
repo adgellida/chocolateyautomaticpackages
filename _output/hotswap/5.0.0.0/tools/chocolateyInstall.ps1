5c6cb268682e781f3d780eb48ae61108554d9390
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-01-22 22:24:53 +0100

    'Updating'

---
+++ _output/hotswap/5.0.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿try {
+	$packageName = 'hotswap'
+	$url = 'http://mt-naka.com/hotswap/file/HotSwap! 5.0.0.0.ZIP' 
+    $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+	$processor = Get-WmiObject Win32_Processor
+	$is64bit = $processor.AddressWidth -eq 64
+	
+    Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+	if ($is64bit) {
+    & "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\64bit\HotSwap!.exe"
+	} else {
+    & "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\32bit\HotSwap!.exe"
+	}
+	
+    Write-ChocolateySuccess $packageName
+}   catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
