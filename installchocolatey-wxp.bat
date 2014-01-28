481d0cb2f8490b19daf85905a9f0b007121f912b
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-01-28 23:20:09 +0100

    'Updating'

--- installchocolatey-wxp.bat
+++ installchocolatey-wxp.bat
@@ -7,5 +7,6 @@ rem ####v.2013.05.30 alpha 16:05 GMT+1
 @powershell Start-Sleep -s 3 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 @powershell Write-Host "All the processes has been realized. Enjoy it!"
-cinst ChocolateyGUI
+rem cinst ChocolateyGUI
+cinst ChocoPM.install -Pre
 pause
