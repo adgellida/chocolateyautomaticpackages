c83ab2d0bffe617f8d90915549a6b9e0c5dd208f
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-02-08 19:41:17 +0100

    'Updating'

---
+++ _output/daemontoolslite/4.48.1.20140208/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'daemontoolslite'
+$installerType = 'EXE'
+$url = 'http://tonigellida.x10.mx/owncloud/public.php?service=files&t=c52c4a68a126fbe043597933f70f4e71&download'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
