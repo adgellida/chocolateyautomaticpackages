3650bbf3aa42dd797ab7e3d943060fee6dd418c8
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-02-12 23:14:20 +0100

    'Updating'

---
+++ _output/clover/3.0.406/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'clover'
+$installerType = 'EXE'
+$url = 'http://ejie.me/uploads/Clover_Setup_3.0.406.zip'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+try {
+  $chocTempDir = Join-Path $env:TEMP 'chocolatey'
+  $tempDir = Join-Path $chocTempDir "$packageName"
+
+  # Download and extract installer
+  Install-ChocolateyZipPackage "$packageName" "$url" $tempDir
+  
+  # Run the installer
+  $installer = Join-Path $tempDir 'clover_setup.exe'
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installer" -validExitCodes $validExitCodes
+  
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
+  throw
+}
