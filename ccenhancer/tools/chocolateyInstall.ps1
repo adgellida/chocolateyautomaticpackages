0f598a4228bbe2d6dfd02be4290e1cec8ff55ac2
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-02-17 23:36:38 +0100

    'Updating'

--- ccenhancer/tools/chocolateyInstall.ps1
+++ ccenhancer/tools/chocolateyInstall.ps1
@@ -4,6 +4,6 @@ $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation
 
-$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\CCEnhancer\CCEnhancer.exe"
+$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\CCEnhancer-$version-Multilanguage\CCEnhancer.exe"
 
 Install-ChocolateyDesktopLink $targetFilePath
