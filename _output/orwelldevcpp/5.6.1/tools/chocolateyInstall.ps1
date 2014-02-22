adec05afb8074f5a58219308b1ab7e7297a3e4ea
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-02-22 01:42:20 +0100

    'Updating'

---
+++ _output/orwelldevcpp/5.6.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'orwelldevcpp'
+$installerType = 'EXE'
+$url = 'http://heanet.dl.sourceforge.net/project/orwelldevcpp/Setup Releases/Dev-Cpp 5.6.1 TDM-GCC x64 4.8.1 Setup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
