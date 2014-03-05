b764b4b428fba98efa0eb8e7c05041f8dd7281d9
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-03-06 00:22:46 +0100

    'Updating'

---
+++ usbit/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\USB Image Tool.exe"
+
+Install-ChocolateyDesktopLink $targetFilePath
+
+
+
+
+
+
+
+
+
+
+
+
+
