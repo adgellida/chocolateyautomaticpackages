1a5e2668fdf629cc21c38fc3d4e919c5c0d12bd1
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-04-05 00:17:54 +0200

    'Updating'

--- peazip/tools/chocolateyInstall.ps1
+++ peazip/tools/chocolateyInstall.ps1
@@ -1,15 +1,9 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-$processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  if ($is64bit) {
-    $url = '{{DownloadUrlx64}}'
-  } else {
-    $url = '{{DownloadUrl}}'
-  }
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
 
