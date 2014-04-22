c4aa9009b84c8caa3f6a08673fcf960e051848f0
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-04-22 02:00:28 +0200

    'Updating'

--- installchocolatey.bat
+++ installchocolatey.bat
@@ -1,12 +1,12 @@
 @echo off
 
 rem ####install chocolatey
-rem ####v.2014.01.28 alpha 23:22 GMT+1
+rem ####v.2014.04.22 alpha 02:00 GMT+1
 
 @powershell Write-Host "Installing Chocolatey, ChocolateyGUI and Nuget Package Explorer... Please wait"
 @powershell Start-Sleep -s 3 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 @powershell Write-Host "All the processes has been realized. Enjoy it!"
-rem cinst ChocolateyGUI
-cinst ChocoPM.install -Pre
+cinst ChocolateyGUI
+rem cinst ChocoPM.install -Pre
 pause
