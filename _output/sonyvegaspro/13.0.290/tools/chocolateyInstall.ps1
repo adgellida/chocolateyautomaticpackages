4ac5973e8628259f30d3c670d7e7acafa621963c
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-04-25 01:22:19 +0200

    'Updating'

---
+++ _output/sonyvegaspro/13.0.290/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'sonyvegaspro'
+$installerType = 'EXE'
+$url = 'http://dspcdn.sonycreativesoftware.com/secure/vegaspro13.0.290_64bit.exe?e=1398168278&h=6c0b0738b094bec97f5eb5a5ea8c933d&file=.exe'
+$silentArgs = '/q /s'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
