4864b3780cc3e91431d73f170b6570a377f9c662
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-05-01 18:10:12 +0200

    'Updating'

---
+++ _output/downloadapp/1.6.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'downloadapp'
+$installerType = 'EXE'
+$url = 'http://download1351.mediafire.com/yyr3057ygadg/g39toc4n66oaxi7/DownloadApp_1_6_6_175d_Setup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
