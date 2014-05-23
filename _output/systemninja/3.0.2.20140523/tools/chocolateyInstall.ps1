181f21b4b510c5d0901754a6962ee383a036bcf2
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-05-23 19:26:52 +0200

    'Updating'

--- _output/systemninja/3.0.2.20140523/tools/chocolateyInstall.ps1
+++ _output/systemninja/3.0.2.20140523/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'systemninja'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'http://download.thewebatom.net/53782d6474d5e/ninja-setup-3.0.2.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
--- _output/systemninja/3.0.2.20140523/tools/chocolateyUninstall.ps1
+++ _output/systemninja/3.0.2.20140523/tools/chocolateyUninstall.ps1
@@ -1,4 +1,4 @@
-$packageName = '{{PackageName}}'
+$packageName = 'systemninja'
 $installerType = 'EXE'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
