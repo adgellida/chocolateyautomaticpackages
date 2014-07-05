4ca3cae0fd80bde5c7992a1257b395cdc73304c1
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-07-05 14:28:04 +0200

    'Updating'

---
+++ _output/minetest/0.4.9/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'minetest'
+$url = 'https://github.com/minetest/minetest/releases/download/0.4.9/minetest-0.4.9-win32-mingw.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\minetest-$version-win32\bin\minetest.exe"
+
+Install-ChocolateyDesktopLink $targetFilePath
