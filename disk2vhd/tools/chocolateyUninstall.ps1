4f4bf06ee19d14871a3b305cc75d1ce014f30e8c
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-07-14 17:21:05 +0200

    Updating

--- disk2vhd/tools/chocolateyUninstall.ps1
+++ disk2vhd/tools/chocolateyUninstall.ps1
@@ -1,7 +1,7 @@
 $packageName = '{{PackageName}}'
 
 try {
-  Remove-Item "$Home\Desktop\arduino.exe.lnk"
+  Remove-Item "$Home\Desktop\disk2vhd.exe"
   # the following is all part of error handling
   Write-ChocolateySuccess "$packageName"
 } catch {
