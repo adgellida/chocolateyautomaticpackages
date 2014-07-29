7e02101a446ffb816a57d2bd494dbea24d8a96fe
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-07-29 10:26:10 +0200

    'Updating'

---
+++ _output/heroesofnewerth/3.2.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'heroesofnewerth'
+$installerType = 'EXE'
+$url = 'http://dl.heroesofnewerth.com/HoNClient-3.2.7.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+if(Test-Path "${Env:ProgramFiles(x86)}\Heroes of Newerth\hon.exe"){
+& "${Env:ProgramFiles(x86)}\Heroes of Newerth\hon.exe" -update
+}
+
+elseif(Test-Path "${Env:ProgramFiles}\Heroes of Newerth\hon.exe"){
+& "${Env:ProgramFiles}\Heroes of Newerth\hon.exe" -update
+}
+
+else{
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+}
