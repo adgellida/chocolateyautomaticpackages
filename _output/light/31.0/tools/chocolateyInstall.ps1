67e12b2ecd93d6541c367f169c8a09d2ea4de4e0
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-02 21:28:36 +0200

    'Updating'

--- _output/light/31.0/tools/chocolateyInstall.ps1
+++ _output/light/31.0/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'light'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'http://freefr.dl.sourceforge.net/project/lightfirefox/31/vc_sync/light-31.0.en-US.win32.installer_sync.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $fileFullPath = "$Env:ProgramFiles\Light\light.exe"
