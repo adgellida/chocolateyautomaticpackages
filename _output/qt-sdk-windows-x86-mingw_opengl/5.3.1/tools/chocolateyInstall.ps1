3d6cdf64526acb849915ad16e063c1422f11d7d5
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-04 23:35:42 +0200

    'Updating'

---
+++ _output/qt-sdk-windows-x86-mingw_opengl/5.3.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'qt-sdk-windows-x86-mingw_opengl'
+$installerType = 'EXE'
+$url = 'http://download.qt-project.org/official_releases/qt/5.3/5.3.1/qt-opensource-windows-x86-mingw482_opengl-5.3.1.exe'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
