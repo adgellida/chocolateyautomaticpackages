4cddebe26c37a843a614884b3004a72dfefd437e
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-09 11:23:18 +0200

    'Updating'

---
+++ _output/line/3.7.3.82/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'line'
+$installerType = 'EXE'
+$url = 'http://dl.desktop.line.naver.jp/naver/LINE/win/LineInst.exe'
+$silentArgs = '/s'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
