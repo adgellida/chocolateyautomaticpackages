3b2d63888da4f60eeb9966f035d44fb11617632e
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-15 19:23:13 +0200

    'Updating'

---
+++ _output/qt-creator-x86/3.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'qt-creator-x86'
+$installerType = 'EXE'
+$url = 'http://download.qt-project.org/official_releases/qtcreator/3.1/3.1.2/qt-creator-opensource-windows-x86-3.1.2.exe'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
