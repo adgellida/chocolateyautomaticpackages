9e52675270fe1e6c66ef35303d8f07d3f1ed6958
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-15 17:25:18 +0200

    'Updating'

---
+++ _output/qt-sdk-windows-x64-mingw_opengl_seh/5.3.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'qt-sdk-windows-x64-mingw_opengl_seh'
+$installerType = 'EXE'
+$url = 'http://skylink.dl.sourceforge.net/project/qtx64/qt-x64/5.3.1/mingw-4.8/seh/qt-5.3.1-x64-mingw482r4-seh-opengl.exe'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
---
+++ aida64extreme/README.md
@@ -0,0 +1,8 @@
+chocolatey-tiled
+================
+![AppVeyor](http://img.shields.io/appveyor/ci/AdrianArroyoCalle/chocolatey-tiled.svg)
+![Downloads](http://img.shields.io/chocolatey/dt/tiled.svg)
+![Version](http://img.shields.io/chocolatey/v/tiled.svg)
+***
+
+Tiled Map Editor Chocolatey Package
