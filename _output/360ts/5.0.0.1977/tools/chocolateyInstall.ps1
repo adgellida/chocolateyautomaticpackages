223db4f9f3051f21cd9fbc22374074efdf348941
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-19 22:04:16 +0200

    'Updating'

---
+++ _output/360ts/5.0.0.1977/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = '360ts'
+$installerType = 'EXE'
+$url = 'http://int.down.360safe.com/totalsecurity/360TS_Setup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
