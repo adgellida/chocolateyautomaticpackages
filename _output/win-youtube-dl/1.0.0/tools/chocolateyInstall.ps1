6a17cd36567b17c074f6cfadb077ed0c33645354
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-22 00:07:06 +0200

    'Updating'

---
+++ _output/win-youtube-dl/1.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'win-youtube-dl'
+$url = 'http://tonigellida.x10.mx/chocolateypackages/win-youtube-dl/win-youtube-dl.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\win-youtube-dl.exe"
+
+Install-ChocolateyDesktopLink $targetFilePath
