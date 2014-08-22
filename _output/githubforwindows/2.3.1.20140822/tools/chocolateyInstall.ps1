a4df52f7950d8bfdfe5a32a2e9da4534c72b2a11
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-22 23:25:42 +0200

    'Updating'

--- _output/githubforwindows/2.3.1.20140822/tools/chocolateyInstall.ps1
+++ _output/githubforwindows/2.3.1.20140822/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$packageName = '{{PackageName}}'
-#$version = '{{PackageVersion}}'
+$packageName = 'githubforwindows'
+#$version = '2.3.1.20140822'
 $version = '2.3.1.1'
 $uninstallRegistryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\5f7eb300e2ea4ebf'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'https://github-windows.s3.amazonaws.com/GitHubSetup.exe'
 $silentArgs = ''
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
@@ -21,26 +21,22 @@ try {
 
         # Download and install the program
 
-		# AUTOIT ANCIENT SCRIPT
+		#AUTOIT ANCIENT SCRIPT
 
 	    $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 	    $installerFile = Join-Path $scriptDir 'githubforwindows.au3'
 
-	    $tempDir = "$env:TEMP\chocolatey\$packageName"
+	    $tempDir = "$env:TEMP\chocolatey\githubforwindows"
 	    if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
 	    $file = Join-Path $tempDir "githubsetup.exe"
-	    Get-ChocolateyWebFile "$packageName" "$file" "$url"
+	    Get-ChocolateyWebFile 'githubforwindows' "$file" 'http://github-windows.s3.amazonaws.com/GitHubSetup.exe'
 	  
 	    write-host "Installing `'$file`' with AutoIt3 using `'$installerFile`'"
 	    $installArgs = "/c autoit3 `"$installerFile`" `"$file`""
 	    Start-ChocolateyProcessAsAdmin "$installArgs" 'cmd.exe'
 	    sleep(15)
-	}
-	
-	Write-ChocolateySuccess $packageName
-	
-} catch {
-
-		Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-		throw 
-}
+	    Write-ChocolateySuccess 'githubforwindows'
+	  } catch {
+	    Write-ChocolateyFailure 'githubforwindows' "$($_.Exception.Message)"
+	    throw 
+	  }
