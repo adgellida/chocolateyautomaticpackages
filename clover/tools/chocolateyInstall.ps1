895ffd5ea082d4743997de55b1877f8477d96a7a
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-08-24 13:54:51 +0200

    'Updating'

--- clover/tools/chocolateyInstall.ps1
+++ clover/tools/chocolateyInstall.ps1
@@ -1,22 +1,62 @@
 ﻿$packageName = '{{PackageName}}'
+#$version = '{{PackageVersion}}'
+$version = '3.0'
+$uninstallRegistryPath_x86 = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Clover"
+$uninstallRegistryPath_x64 = "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Clover"
 $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$mantainer = 'tonigellida'
 
 try {
-  $chocTempDir = Join-Path $env:TEMP 'chocolatey'
-  $tempDir = Join-Path $chocTempDir "$packageName"
 
-  # Download and extract installer
-  Install-ChocolateyZipPackage "$packageName" "$url" $tempDir
+	$processor = Get-WmiObject Win32_Processor
+	$is64bit = $processor.AddressWidth -eq 64
+	
+	if ($is64bit) {
+	
+ 		if (Test-Path $uninstallRegistryPath_x64) {
+			$installedVersion = (Get-ItemProperty $uninstallRegistryPath_x64).DisplayVersion
+		}
+
+	} else {
   
-  # Run the installer
-  $installer = Join-Path $tempDir 'clover_setup.exe'
-  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installer" -validExitCodes $validExitCodes
+		if (Test-Path $uninstallRegistryPath_x86) {
+			$installedVersion = (Get-ItemProperty $uninstallRegistryPath_x86).DisplayVersion
+		}	
+		
+	}
+	
+	if ($installedVersion -gt $version) {
+		Write-Host "Your $packageName $installedVersion is higher than the $version proporcionated by chocolatey repo."
+		Write-Host "Please wait or contact the mantainer $mantainer to update this package."
+		Write-Host "When the package is updated try another time. Thanks."
+	}
+	
+	if ($installedVersion -eq $version) {
+		Write-Host "$packageName $version is already installed."
+		
+	} else {
+
+        # Download and install the program
+
+		$chocTempDir = Join-Path $env:TEMP 'chocolatey'
+		$tempDir = Join-Path $chocTempDir "$packageName"
+
+		# Download and extract installer
+		Install-ChocolateyZipPackage "$packageName" "$url" $tempDir
   
-  Write-ChocolateySuccess "$packageName"
+		# Run the installer
+		$installer = Join-Path $tempDir 'clover_setup.exe'
+		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$installer" -validExitCodes $validExitCodes
+		
+		}
+	
+	Write-ChocolateySuccess $packageName
+	
 } catch {
-  Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
-  throw
+
+		Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+		throw 
 }
