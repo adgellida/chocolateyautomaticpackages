4b14a5c22b7c264e377c5995b67aca63de187127
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-09-05 23:24:11 +0200

    'Updating'

---
+++ _output/waterfox/32.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'waterfox'
+$installerType = 'EXE'
+$url = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=waterfox&DownloadId=904894&FileTime=130543234930700000&Build=20928'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
