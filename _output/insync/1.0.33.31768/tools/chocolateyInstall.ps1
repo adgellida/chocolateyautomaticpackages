2974fad8d1d104a3b573e012e184ce89d7b0bdb8
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-09-11 23:29:05 +0200

    'Updating'

---
+++ _output/insync/1.0.33.31768/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'insync'
+$installerType = 'EXE'
+$url = 'http://s.insynchq.com/builds/Insync-1.0.33.31768.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
