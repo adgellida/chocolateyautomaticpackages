4c5f0c065c55e5da221d1118ee747cd3dd0002c1
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-09-24 00:14:42 +0200

    'Updating'

---
+++ _output/spideroak/5.1.8.20140925/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'spideroak'
+$installerType = 'EXE'
+$url = 'http://tonigellida.x10.mx/chocolateypackages/spideroak/SpiderOakSetup-5.1.8.exe'
+$url64 = 'http://tonigellida.x10.mx/chocolateypackages/spideroak/SpiderOakSetup-5.1.8-x64.exe'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
