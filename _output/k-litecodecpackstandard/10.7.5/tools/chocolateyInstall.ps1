f58060c401efbc7fae1d9bdde0a61f3609ccf5b3
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-09-25 00:47:49 +0200

    'Updating'

---
+++ _output/k-litecodecpackstandard/10.7.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'k-litecodecpackstandard'
+$installerType = 'EXE'
+$url = 'http://files01.techspot.com/temp/K-Lite_Codec_Pack_1075_Standard.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+	
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
