df497d8b8c8867399dc9a890a7a59edcc456591b
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-09-27 22:22:21 +0200

    'Updating'

---
+++ _output/driverbooster/1.5.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'driverbooster'
+$installerType = 'EXE'
+$url = 'http://download.instalki.info/programy/Windows/Narzedzia/zarzadzanie_sterownikami/driver_booster_setup_www.INSTALKI.pl.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
