7ddfeeb5c0dfd86e3da8b5938c432863fc2cca3a
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-10-08 21:38:05 +0200

    'Updating'

---
+++ _output/aida64-business/{version}/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'aida64-business'
+$url = 'http://download.aida64.com/aida64business470.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$executable = "aida64.exe"
+$shortcut_to_modify = "$Home\Desktop\aida64.exe.lnk"
+$shortcut_modified = "$Home\Desktop\AIDA64 Business.lnk" 
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$unzipLocation\$executable"
+
+if (Test-Path $shortcut_modified)
+	Remove-Item $shortcut_modified
+
+Install-ChocolateyDesktopLink $targetFilePath
+
+Rename-Item $shortcut_to_modify $shortcut_modified
---
+++ _output/aida64-networkaudit/{version}/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'aida64-networkaudit'
+$url = 'http://download.aida64.com/aida64networkaudit470.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$executable = "aida64.exe"
+$shortcut_to_modify = "$Home\Desktop\aida64.exe.lnk"
+$shortcut_modified = "$Home\Desktop\AIDA64 Network Audit.lnk" 
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$targetFilePath = "$unzipLocation\$executable"
+
+if (Test-Path $shortcut_modified)
+	Remove-Item $shortcut_modified
+
+Install-ChocolateyDesktopLink $targetFilePath
+
+Rename-Item $shortcut_to_modify $shortcut_modified
