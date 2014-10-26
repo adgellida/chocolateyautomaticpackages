3424e58839cc2e57571f5058e70540d234064f2f
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-10-26 11:24:42 +0100

    'Updating'

--- _output/andy/0.41.1010.19442/tools/chocolateyUninstall.ps1
+++ _output/andy/0.41.1010.19442/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'andy'
 $installerType = 'EXE'
-$silentArgs = ''
+$silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $unpath = "$Env:ProgramFiles\Andy\AndyUninstall.exe"
 
--- andy/tools/chocolateyUninstall.ps1
+++ andy/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $packageName = '{{PackageName}}'
 $installerType = 'EXE'
-$silentArgs = ''
+$silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $unpath = "$Env:ProgramFiles\Andy\AndyUninstall.exe"
 
