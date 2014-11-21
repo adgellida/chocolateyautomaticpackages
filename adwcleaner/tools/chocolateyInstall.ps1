19d7d126f45247eb7b0ddebd506da90dea6c7556
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-21 10:25:12 +0100

    'Updating'

--- adwcleaner/tools/chocolateyInstall.ps1
+++ adwcleaner/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
+$version = '{{PackageVersion}}'
 $url = '{{DownloadUrl}}'
-$fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\adwcleaner.exe"
-$shortcut_to_modify = "$Home\Desktop\adwcleaner.exe.lnk"
+$fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\adwcleaner_$version.exe"
+$shortcut_to_modify = "$Home\Desktop\adwcleaner_$version.exe.lnk"
 $shortcut_modified = "$Home\Desktop\AdwCleaner.lnk"
 
 Get-ChocolateyWebFile $packageName $fileFullPath $url
