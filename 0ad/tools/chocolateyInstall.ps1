700b874dd8ce757617945411e4ec76174e560344
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-24 16:49:19 +0100

    'Updating'

---
+++ 0ad/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+
+$fileFullPath = "$Env:userprofile\AppData\Local\0 A.D. alpha\binaries\system\pyrogenesis.exe"
+
+Install-ChocolateyDesktopLink $fileFullPath
