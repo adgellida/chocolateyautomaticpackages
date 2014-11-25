7df9f9895685fa527c4439c2e3dd5164cf156882
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-26 00:33:49 +0100

    'Updating'

--- xonotic/tools/chocolateyUninstall.ps1
+++ xonotic/tools/chocolateyUninstall.ps1
@@ -1,11 +1,21 @@
 $packageName = '{{PackageName}}'
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
 
 try {
-  Remove-Item "$Home\Desktop\xonotic.exe.lnk"
-  Remove-Item "$Home\Desktop\xonotic-x64.exe.lnk"
-  # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
+
+	if ($is64bit) {
+    $shortcut_to_remove = "xonotic-x64.exe.lnk"
+	} else {
+    $shortcut_to_remove = "xonotic.exe.lnk"
+	}
+
+	Remove-Item "$desktop\$shortcut_to_remove"
+  
+	Write-ChocolateySuccess $packageName
+	
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw 
 }
