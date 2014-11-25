e82cf78a243dcd4c4096096a11ba79175ee453c2
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-26 00:25:11 +0100

    'Updating'

--- wincontig/tools/chocolateyInstall.ps1
+++ wincontig/tools/chocolateyInstall.ps1
@@ -2,23 +2,25 @@
 $url = '{{DownloadUrl}}'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
-
 try {
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  if ($is64bit) {
-    $targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\WinContig64.exe"
-  } else {
-    $targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\WinContig.exe"
-  }
-  Install-ChocolateyDesktopLink $targetFilePath
+
+	Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+	$processor = Get-WmiObject Win32_Processor
+	$is64bit = $processor.AddressWidth -eq 64
+	if ($is64bit) {
+		$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\WinContig64.exe"
+	} else {
+		$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\WinContig.exe"
+	}
+	
+	Install-ChocolateyDesktopLink $targetFilePath
   
-  # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+	# the following is all part of error handling
+    Write-ChocolateySuccess $packageName
+}   catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
 }
 
 
--- xonotic/tools/chocolateyInstall.ps1
+++ xonotic/tools/chocolateyInstall.ps1
@@ -1,11 +1,12 @@
-﻿try {
-	$packageName = '{{PackageName}}'
-	$url = '{{DownloadUrl}}' 
-    $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-	$processor = Get-WmiObject Win32_Processor
-	$is64bit = $processor.AddressWidth -eq 64
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}' 
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+
+try {
 	
-    Install-ChocolateyZipPackage $packageName $url $unzipLocation
+	Install-ChocolateyZipPackage $packageName $url $unzipLocation
 	
 	if ($is64bit) {
     $targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\Xonotic\xonotic-x64.exe"
@@ -14,7 +15,8 @@
 	}
     
     Install-ChocolateyDesktopLink $targetFilePath
-
+	
+	# the following is all part of error handling
     Write-ChocolateySuccess $packageName
 }   catch {
     Write-ChocolateyFailure $packageName $($_.Exception.Message)
