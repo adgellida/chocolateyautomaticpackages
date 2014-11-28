11ff81c99cea7038bbe0e0cdf5905e7d9d70c9cc
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-28 13:55:44 +0100

    'Updating'

--- 0ad/tools/chocolateyInstall.ps1
+++ 0ad/tools/chocolateyInstall.ps1
@@ -2,9 +2,18 @@
 $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/S'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $fileFullPath = "$Env:userprofile\AppData\Local\0 A.D. alpha\binaries\system\pyrogenesis.exe"
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+try {
 
-Install-ChocolateyDesktopLink $fileFullPath
+	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+
+	Install-ChocolateyDesktopLink $fileFullPath
+	
+	Write-ChocolateySuccess $packageName
+	
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw 
+}
--- 0ad/tools/chocolateyUninstall.ps1
+++ 0ad/tools/chocolateyUninstall.ps1
@@ -1,20 +1,20 @@
 $packageName = '{{PackageName}}'
 $installerType = 'EXE'
 $silentArgs = '/S'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
 $shortcut_to_remove = "pyrogenesis.exe.lnk"
 $unpath = "$Env:userprofile\AppData\Local\0 A.D. alpha\Uninstall.exe"
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 try {
 
-  Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
+	Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $unpath -validExitCodes $validExitCodes
 
-  Remove-Item "$desktop\$shortcut_to_remove"
+	Remove-Item "$desktop\$shortcut_to_remove"
   
-  # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
+	Write-ChocolateySuccess $packageName
+	
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw 
 }
