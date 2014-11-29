fb0e6c26bce63448b0b1bc528e5e79aeb4f48f4f
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-29 11:02:21 +0100

    'Updating'

--- tdesktop/tools/chocolateyInstall.ps1
+++ tdesktop/tools/chocolateyInstall.ps1
@@ -1,38 +1,16 @@
 ﻿$packageName = '{{PackageName}}'
-$version = '{{PackageVersion}}'
-$exeToVersioning = "$Env:userprofile\AppData\Roaming\Telegram Desktop\Telegram.exe"
 $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-$mantainer = 'tonigellida'
 
 try {
 
- 	if (Test-Path $exeToVersioning) {
-		$installedVersion = (Get-Command $exeToVersioning).FileVersionInfo.FileVersion
-	}
-
-	if ($installedVersion -gt $version) {
-		Write-Host "Your $packageName $installedVersion is higher than the $version proporcionated by chocolatey repo."
-		Write-Host "Please wait or contact the mantainer $mantainer to update this package."
-		Write-Host "When the package is updated try another time. Thanks."
-		
-	}elseif ($installedVersion -eq $version) {
-		Write-Host "$packageName $version is already installed."
-		
-	} else {
-
-        # Download and install the program
-
-		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
-		
-		}
+	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
 	
 	Write-ChocolateySuccess $packageName
 	
 } catch {
-
-		Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-		throw 
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw 
 }
--- tdesktop/tools/chocolateyUninstall.ps1
+++ tdesktop/tools/chocolateyUninstall.ps1
@@ -1,16 +1,17 @@
 $packageName = '{{PackageName}}'
 $installerType = 'EXE'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$unpath = "$Env:userprofile\AppData\Roaming\Telegram Desktop\unins000.exe"
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-$unpath = "$Env:userprofile\AppData\Roaming\Telegram Win (Unofficial)\unins000.exe"
 
 try {
 
-  Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
+	Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
   
-  # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
+	Write-ChocolateySuccess $packageName
+	
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw 
 }
+
