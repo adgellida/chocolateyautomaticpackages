3cb4118bc218dd9f9fd9d097cedfd7f3e06d4a35
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-29 13:58:56 +0100

    'Updating'

--- _output/windowsrepair/2.10.2/tools/chocolateyInstall.ps1
+++ _output/windowsrepair/2.10.2/tools/chocolateyInstall.ps1
@@ -4,4 +4,13 @@ $url = 'http://www.tweaking.com/files/setups/tweaking.com_windows_repair_aio_set
 $silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+try {
+
+	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+	
+	Write-ChocolateySuccess $packageName
+	
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw 
+}
