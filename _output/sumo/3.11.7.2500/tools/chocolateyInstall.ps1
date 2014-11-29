a5a275f0d5cdd652b5add55fd759f2446ce4e174
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-29 14:58:55 +0100

    'Updating'

--- _output/sumo/3.11.7.2500/tools/chocolateyInstall.ps1
+++ _output/sumo/3.11.7.2500/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'sumo'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.kcsoftwares.com/files/sumo_lite.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
