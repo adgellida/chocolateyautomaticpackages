0a6546991540eb7f0e67410771852bbb89590130
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-11-30 11:37:43 +0100

    'Updating'

--- opswatsecurityscore/tools/chocolateyInstall.ps1
+++ opswatsecurityscore/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
 $installerType = 'msi'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/qb'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$validExitCodes = @(0,3010) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
--- smartftp/tools/chocolateyInstall.ps1
+++ smartftp/tools/chocolateyInstall.ps1
@@ -3,6 +3,6 @@ $installerType = 'msi'
 $url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
 $silentArgs = '/quiet'
-$validExitCodes = @(0, 3010) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$validExitCodes = @(0,3010) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
