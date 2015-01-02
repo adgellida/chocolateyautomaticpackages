2f94a5776e842606dfaecd973e499c7aaa5075b7
aronovgj <aronovgj@gmx.net>
2015-01-02 05:49:54 +0100

    removing packages
    removing packages according to:
    https://github.com/chocolatey/chocolatey/wiki/PackageMantainerHandover
    
    with link to the new repo

--- _output/cpu-z/1.65/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'cpu-z'
-$installerType = 'EXE'
-#$url = 'ftp://ftp.cpuid.com/cpu-z/cpu-z_{version}-setup-en.exe'
-$url = 'ftp://ftp.cpuid.com/cpu-z/cpu-z_1.65-setup-en.exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
