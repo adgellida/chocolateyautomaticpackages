d06883331518737226c30ce2f0d6240d4741f773
aronovgj <aronovgj@gmx.net>
2015-01-02 06:33:04 +0100

    removing packages
    removing packages according to:
    https://github.com/chocolatey/chocolatey/wiki/PackageMantainerHandover
    
    with link to the new repo

--- _output/ccenhancer/3.9.20140217/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'ccenhancer'
-$url = 'http://download.thewebatom.net/5300a256e895a/CCEnhancer-3.9-Multilanguage.zip'
-$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
-
-$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\CCEnhancer-3.9-Multilanguage\CCEnhancer.exe"
-
-Install-ChocolateyDesktopLink $targetFilePath
---
+++ _output/ccleaner/4.19.4867/tools/chocolateyInstall.ps1
@@ -0,0 +1,52 @@
+﻿$packageName = 'ccleaner'
+$versionToConvert = '4.19.4867'
+$version = $versionToConvert.Substring(0,$versionToConvert.Length-5)
+$uninstallRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\CCleaner"
+$fileType = 'exe'
+$LCID = (Get-Culture).LCID
+$url = 'http://download.piriform.com/ccsetup419.exe'
+$silentArgs = "/S /L=$LCID"
+# Please test every new version of CCleaner for possible adware/spyware/crapware which installs silently together with Piriform software products.
+# Only push the new package to the gallery if you are 100% sure that this package prevents the install of the bundled adware.
+$regAdd = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\regAdd.ps1"
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$mantainer = 'tonigellida'
+
+try {
+ 		if (Test-Path $uninstallRegistryPath) {
+			$installedVersion = (Get-ItemProperty $uninstallRegistryPath).DisplayVersion
+		}
+	
+	if ($installedVersion -gt $version) {
+		Write-Host "Your $packageName $installedVersion is higher than the $version proporcionated by chocolatey repo."
+		Write-Host "Please wait or contact the mantainer $mantainer to update this package."
+		Write-Host "When the package is updated try another time. Thanks."
+		
+	}elseif ($installedVersion -eq $version) {
+		Write-Host "$packageName $version is already installed."
+		
+	} else {
+
+        # Download and install the program
+
+		# This adds a registry key which prevents Google Chrome from getting installed together with Piriform software products.
+		Start-ChocolateyProcessAsAdmin "& `'$regAdd`'"
+
+		Install-ChocolateyPackage $packageName $fileType $silentArgs $url -validExitCodes $validExitCodes
+		
+		}
+	
+	Write-ChocolateySuccess $packageName
+	
+} catch {
+
+		Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+		throw 
+}
+
+
+
+
+
+
+
