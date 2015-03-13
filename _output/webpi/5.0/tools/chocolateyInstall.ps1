71216d7afca42f126f17e2f4199cdc2cf2b6ec3b
dtgm <dtgm1594@gmail.com>
2015-03-13 18:53:30 -0500

    package handover: webpi https://github.com/adgellida/chocolateyautomaticpackages/pull/10

--- _output/webpi/5.0/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'webpi'
-$installerType = 'EXE'
-$url = 'http://go.microsoft.com/fwlink/?LinkId=255386'
-$silentArgs = ''
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
