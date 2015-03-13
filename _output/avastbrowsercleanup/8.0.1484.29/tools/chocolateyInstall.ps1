34419759a0e3f3d45b788afae74a20a3a59bce61
dtgm <dtgm1594@gmail.com>
2015-03-13 18:55:35 -0500

    package handover: avastbrowsercleanup, avastfreeantivirus https://github.com/adgellida/chocolateyautomaticpackages/pull/10

--- _output/avastbrowsercleanup/8.0.1484.29/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'avastbrowsercleanup'
-#$url = 'http://files.avast.com/files/tools/avast-browser-cleanup.exe'
-$url = 'http://files.avast.com/files/tools/avast-browser-cleanup.exe'
-$fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\avast-browser-cleanup.exe"
-
-Get-ChocolateyWebFile $packageName $fileFullPath $url
-
-Install-ChocolateyDesktopLink $fileFullPath
