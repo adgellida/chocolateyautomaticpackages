cb8f14fa9a4e962ae0d69c6fa428f5f0904392fc
dtgm <dtgm1594@gmail.com>
2015-06-13 15:32:36 -0500

    package handover: aida64-business/-engineer/-extreme/-networkaudit
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

--- _output/aida64-networkaudit/4.60.3000/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'aida64-networkaudit'
-$url = 'http://download.aida64.com/aida64networkaudit460.zip'
-$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
-
-$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\aida64.exe"
-
-Install-ChocolateyDesktopLink $targetFilePath
