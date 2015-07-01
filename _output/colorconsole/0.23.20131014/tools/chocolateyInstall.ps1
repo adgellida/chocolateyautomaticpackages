c9f286c510de45744db3880314e15fc871cf8445
dtgm <dtgm1594@gmail.com>
2015-07-01 13:25:02 -0500

    package handover: cccp, colorconsole, disk2vhd, drivergenius, partitionwizard, softwareinformer, systemninja
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

--- _output/colorconsole/0.23.20131014/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'colorconsole'
-$url = 'http://www.softwareok.com/Download/ColorConsole.zip'
-$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-Install-ChocolateyZipPackage $packageName $url $unzipLocation
-
-$targetFilePath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\ColorConsole.exe"
-
-Install-ChocolateyDesktopLink $targetFilePath
