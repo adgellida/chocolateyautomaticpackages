a43fdc4b15b12e8e0de496d611ebce499cdec85b
Antonio David Gellida Lavara <adgellida@gmail.com>
2017-06-16 22:13:37 +0200

    "Updating"

---
+++ winsys/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'winsys'
+$installerType = 'EXE'
+$silentArgs = '--mode unattended'
+$url = 'https://github.com/adgellida/winsys/releases/download/v2017.06.16/winsys-2017.06.16-windows-installer.exe'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
