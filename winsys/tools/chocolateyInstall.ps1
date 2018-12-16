687eb5cb428b935ac09b8dacaa16667211e500d3
Antonio David Gellida Lavara <adgellida@gmail.com>
2018-12-16 20:33:30 +0100

    "Updating"

--- winsys/tools/chocolateyInstall.ps1
+++ winsys/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
  
 $toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $PackageParameters   = Get-PackageParameters
-$urlPackage          = 'https://github.com/adgellida/winsys/releases/download/v2018.12.08/winsys-2018.12.08-windows-installer.exe'
-$checksumPackage     = '4FAE77B3862DDA779831E4037D3871635979F7A442D8B10021A598E0DFE2D5D7B5A91325FB9945C4DFDCCD6148643AECBEDE4542A599881D12CE656D536EBDD8'
+$urlPackage          = 'https://github.com/adgellida/winsys/releases/download/v2018.12.08/winsys-2018.12.16-windows-installer.exe'
+$checksumPackage     = 'A3FE8049F316853E85DC84A572E0C08BA5CABC396A67E3B97A74F53871633B26E8CC42F9F6FCC26602AEA6585DE1131FF6F0DBBF4B098BD16BC8B2270AE7D4F3'
 $checksumTypePackage = 'SHA512'
  
 $packageArgs = @{
